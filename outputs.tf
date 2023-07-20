14e6e01a272dbc8fbc5d1f4fb1b8e9e36a64c161
John Smoker <jsmoker@aviatrix.com>
2023-07-20 11:19:52 -0400

    fix output name for version used

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,6 @@ output "lb_dns_name" {
   value = aws_lb.centralized_ingress_lb.dns_name
 }
 
-output "sg_ids" {
-  value = {
-    lb    = module.security_group_centralized_lb.*
-    proxy = module.security_group_proxy_vm.*
-    web   = module.security_group_web_vm.*
-  }
+output "lb_sg_id" {
+  value = module.security_group_centralized_lb.this_security_group_id
 }
