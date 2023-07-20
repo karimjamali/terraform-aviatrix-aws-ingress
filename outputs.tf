1a387c476d9f754e32ac2e6ad497d487a14f4d51
John Smoker <jsmoker@aviatrix.com>
2023-07-20 10:50:53 -0400

    wrong output reference

--- outputs.tf
+++ outputs.tf
@@ -4,8 +4,8 @@ output "lb_dns_name" {
 
 output "sg_ids" {
   value = {
-    lb    = module.security_group_centralized_lb.outputs.security_group_id
-    proxy = module.security_group_proxy_vm.outputs.security_group_id
-    web   = module.security_group_web_vm.outputs.security_group_id
+    lb    = module.security_group_centralized_lb.*.security_group_id
+    proxy = module.security_group_proxy_vm.*.security_group_id
+    web   = module.security_group_web_vm.*.security_group_id
   }
 }
