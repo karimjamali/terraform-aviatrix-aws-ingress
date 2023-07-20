80fee0dd183b84ce71f7f5191216bc396983e611
John Smoker <jsmoker@aviatrix.com>
2023-07-20 10:34:03 -0400

    add sg ids to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "lb_dns_name" {
   value = aws_lb.centralized_ingress_lb.dns_name
 }
+
+output "sg_ids" {
+  value = {
+    lb    = module.security_group_centralized_lb.security_group_id
+    proxy = module.security_group_proxy_vm.security_group_id
+    web   = module.security_group_web_vm.security_group_id
+  }
+}
