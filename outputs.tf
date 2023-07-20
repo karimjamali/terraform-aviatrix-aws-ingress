2dc0088248d18a616cffc3618130477de25a0d62
John Smoker <jsmoker@aviatrix.com>
2023-07-20 10:52:24 -0400

    this can't work

--- outputs.tf
+++ outputs.tf
@@ -4,8 +4,8 @@ output "lb_dns_name" {
 
 output "sg_ids" {
   value = {
-    lb    = module.security_group_centralized_lb.*.security_group_id
-    proxy = module.security_group_proxy_vm.*.security_group_id
-    web   = module.security_group_web_vm.*.security_group_id
+    lb    = module.security_group_centralized_lb.*
+    proxy = module.security_group_proxy_vm.*
+    web   = module.security_group_web_vm.*
   }
 }
